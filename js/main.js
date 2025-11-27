6064d14c049f496d9d4175cb94197eec39531fbd
disowsa <disowsa@gmail.com>
2025-11-27 11:42:23 +0000

    Changes before Firebase Studio auto-run

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.side-menu').style.right = '0';
 });
 
+document.querySelector('.close-btn').addEventListener('click', () => {
+    document.querySelector('.side-menu').style.right = '-250px';
+});
+
 document.addEventListener('click', (e) => {
     if (!e.target.closest('.side-menu') && !e.target.closest('.hamburger')) {
         document.querySelector('.side-menu').style.right = '-250px';
